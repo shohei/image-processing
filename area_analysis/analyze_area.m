014f4d1d58b586bfab241274494f0a8da201e1ba
Shohei Aoki <shoaok@gmail.com>
2016-07-08 15:24:51 +0900

    add result

--- area_analysis/analyze_area.m
+++ area_analysis/analyze_area.m
@@ -3,54 +3,86 @@ function analyze_area
 clear; close all;
 tic;
 
-X0 = imread('cell.jpg');
-size(X0)
-subplot(331);
-imagesc(X0);
-axis image;
-axis off;
-title('raw');
-
-Xgray = (double(X0(:,:,1))+double(X0(:,:,2))+double(X0(:,:,3)))/3;
-X=Xgray;
-Xscale= (X-min(X(:)))/(max(X(:))-min(X(:)))*1;
-X=Xscale;
-%binarize
-X(X>=0.6)=1;
-X(X<0.6)=0;
-subplot(332);
-imagesc(X);
-axis image;
-axis off;
-title('binarized');
-colormap(gray);
-freezeColors;
-toc;
-
-Y=label_area(X);
-toc;
-
-n_lab = max(Y(:));
-subplot(333);
-imagesc(Y);
-axis image;
-axis off;
-NL = n_lab;
-colormap(jet);
-title('label');
-freezeColors;
-[M,N]=size(Y);
-for label=1:NL
-    Z=zeros(M,N);
-    for m=1:M
-        for n=1:N
-            if Y(m,n)==label
-                Z(m,n) = 1;
+if ~(exist('Ydata.mat', 'file') == 2)
+    
+    X0 = imread('cell.jpg');
+    size(X0)
+    subplot(331);
+    imagesc(X0);
+    axis image;
+    axis off;
+    title('raw');
+    
+    Xgray = (double(X0(:,:,1))+double(X0(:,:,2))+double(X0(:,:,3)))/3;
+    X=Xgray;
+    Xscale= (X-min(X(:)))/(max(X(:))-min(X(:)))*1;
+    X=Xscale;
+    %binarize
+    X(X>=0.6)=1;
+    X(X<0.6)=0;
+    subplot(332);
+    imagesc(X);
+    axis image;
+    axis off;
+    title('binarized');
+    colormap(gray);
+    freezeColors;
+    toc;
+    
+    Y=label_area(X);
+    toc;
+    
+    n_lab = max(Y(:));
+    subplot(333);
+    imagesc(Y);
+    axis image;
+    axis off;
+    NL = n_lab;
+    colormap(jet);
+    title('label');
+    freezeColors;
+    [M,N]=size(Y);
+    for label=1:NL
+        Z=zeros(M,N);
+        for m=1:M
+            for n=1:N
+                if Y(m,n)==label
+                    Z(m,n) = 1;
+                end
             end
         end
+        area(label)=sum(Z(:));
     end
-    area(label)=sum(Z(:));
+    
+    save('Ydata.mat');
+
+else
+    load('Ydata.mat');
+
+    subplot(331);
+    imagesc(X0);
+    axis image;
+    axis off;
+    title('raw');
+
+    subplot(332);
+    imagesc(X);
+    axis image;
+    axis off;
+    title('binarized');
+    colormap(gray);
+    freezeColors;
+
+    subplot(333);
+    imagesc(Y);
+    axis image;
+    axis off;
+    colormap(jet);
+    title('label');
+    freezeColors;
+    
 end
+
 
 subplot(3,3,[4 5 6]);
 bar(area);
@@ -61,12 +93,12 @@ toc;
 lower=201;
 upper=6000;
 Yout= Y*0;
-Yout=isolateArea(Yout,lower,upper);
+Yo =isolateArea(Y,Yout,lower,upper);
 subplot(337);
-imagesc(uint8(Yout));
+imagesc(uint8(Yo));
 axis image;
 axis off;
-colormap(jet);
+colormap(prism);
 title('greater than 201');
 freezeColors;
 toc;
@@ -75,11 +107,11 @@ lower=1001;
 upper=6000;
 Yout= Y*0;
 subplot(338);
-Yout=isolateArea(Yout,lower,upper);
-imagesc(uint8(Yout));
+Yo =isolateArea(Y,Yout,lower,upper);
+imagesc(uint8(Yo));
 axis image;
 axis off;
-colormap(jet);
+colormap(prism);
 title('greater than 1001');
 freezeColors;
 toc;
@@ -88,22 +120,23 @@ lower=51;
 upper=6000;
 Yout= Y*0;
 subplot(339);
-Yout=isolateArea(Yout,lower,upper);
-imagesc(uint8(Yout));
+Yo =isolateArea(Y,Yout,lower,upper);
+imagesc(uint8(Yo));
 axis image;
 axis off;
-colormap(jet);
+colormap(prism);
 title('greater than 51');
 freezeColors;
 toc;
 
 
-    function Yout=isolateArea(Yout,lower,upper)
+    function Yout=isolateArea(Y,Yout,lower,upper)
         for iLabel=1:NL
             for m=1:M
                 for n=1:N
                     if Y(m,n)==iLabel
                         YY(m,n) = 1;
+                    else
                         YY(m,n) = 0;
                     end
                 end
